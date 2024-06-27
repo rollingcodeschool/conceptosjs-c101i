6edf6f391680eefc0c978113fbaf42fe8a1fd430
Emilse Arias <emilsearias@gmail.com>
2024-06-27 17:05:29 -0300

    creamos dos variables con let y las mostramos por consola y en el documento html

---
+++ js/01-variables.js
@@ -0,0 +1,22 @@
+//Esto es un comentario de una linea
+
+/* Esto es un comentario
+de
+multiples
+lineas 
+*/
+
+// declarar o crear una variable (var - let - const)
+
+let edad = 30;
+let encendido = false;
+
+// mostrar el contenido de una variable
+console.log(edad);
+document.write("<p class='textoResaltado' >hola mundo</p>")
+document.write("Edad: " + edad + "<br>")
+
+//modificar el contenido de una variable
+
+edad = 30 + 3;
+document.write("Edad: " + edad)
