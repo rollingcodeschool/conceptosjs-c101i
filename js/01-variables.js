b308b54aa74e6b1feb50235130e291584541f577
Emilse Arias <emilsearias@gmail.com>
2024-07-02 16:04:14 -0300

    agregamos un ejemplo de ventanas emergentes

--- js/01-variables.js
+++ js/01-variables.js
@@ -10,13 +10,20 @@ lineas
 
 let edad = 30;
 let encendido = false;
+let nombreApellido= 'nombre'
 
 // mostrar el contenido de una variable
 console.log(edad);
-document.write("<p class='textoResaltado' >hola mundo</p>")
+document.write("<p class='textoResaltado' >hola 'mundo'</p>")
 document.write("Edad: " + edad + "<br>")
 
 //modificar el contenido de una variable
 
 edad = 30 + 3;
 document.write("Edad: " + edad)
+
+// mostrar en ventanas emergentes
+alert("bienvenidos")
+
+const edadUsuario = prompt("Ingrese su edad")
+console.log(edadUsuario)
