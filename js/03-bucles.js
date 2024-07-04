186c049b359b1f9d790ef2f02694fdedf7a599c5
Emilse Arias <emilsearias@gmail.com>
2024-07-04 16:37:54 -0300

    agregamos un ejemplo de bucle while

---
+++ js/03-bucles.js
@@ -0,0 +1,13 @@
+// bucles while, do-while, for
+
+// while(condicion logica){
+    // todas las lineas de codigo que quiero repetir
+    // agregar una linea que haga que la condicion logica se deje de cumplir
+// }
+
+
+let numero = 1;
+while(numero<=10){
+    document.write(`<p>Linea número ${numero}</p>`)
+    numero =  numero + 1;
+}
