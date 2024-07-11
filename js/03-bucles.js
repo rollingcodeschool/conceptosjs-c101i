331848d3828b350c7168a8a4621c0eb06ea98b8c
Emilse Arias <emilsearias@gmail.com>
2024-07-11 15:59:51 -0300

    agregamos un ejemplo de estructura for

--- js/03-bucles.js
+++ js/03-bucles.js
@@ -1,13 +1,36 @@
 // bucles while, do-while, for
 
 // while(condicion logica){
-    // todas las lineas de codigo que quiero repetir
-    // agregar una linea que haga que la condicion logica se deje de cumplir
+// todas las lineas de codigo que quiero repetir
+// agregar una linea que haga que la condicion logica se deje de cumplir
 // }
 
+let numero = 100;
+while (numero <= 10) {
+  document.write(`<p>Linea número ${numero}</p>`);
+  numero = numero + 1;
+}
 
-let numero = 1;
-while(numero<=10){
-    document.write(`<p>Linea número ${numero}</p>`)
-    numero =  numero + 1;
+/* 
+do{
+    todas las lineas de codigo que quiero repetir
+    agregar una linea que haga que la condicion logica se deje de cumplir
+}while(condicion logica)
+*/
+
+let renglon = 1;
+
+do {
+  document.write(`<p>Renglon número ${renglon}</p>`);
+  renglon++;  //renglon = renglon + 1; 
+} while (renglon <= 5);
+
+/*
+for(inicializar una variable; condicion logica; incrementar/decrementar variable){
+    todas las lineas de codigo que quiero repetir
+}
+*/ 
+
+for(let contador = 10; contador >= 1 ; contador--){
+    document.write(`<p>Cuenta regresiva N°: ${contador}</p>`);
 }
