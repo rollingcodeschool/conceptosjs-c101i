9380f2a413ebd3c52d84e88f56d4cde53bba514e
Emilse Arias <emilsearias@gmail.com>
2024-07-18 16:45:51 -0300

    agregamos una funcion para mostrar los datos del array de forma mas ordenada

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -11,6 +11,17 @@ const series = [
   "bridgerton",
 ];
 
+function mostrarSeries() {
+  document.write(
+    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
+  );
+  document.write(`<ul>`);
+  for (let i = 0; i < series.length; i++) {
+    document.write(`<li>${series[i]}</li>`);
+  }
+  document.write(`</ul>`);
+}
+
 console.log(peliculas);
 console.log(series);
 //cantidad de elementos en el array
@@ -24,95 +35,34 @@ document.write(
 document.write(`<p>Array de series: ${series}</p>`);
 
 //mostrar los elementos del array
-document.write(
-  `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-);
-document.write(`<ul>`);
-for(let i=0; i < series.length; i++){
-    document.write(`<li>${series[i]}</li>`);
-}
-document.write(`</ul>`);
+mostrarSeries();
 
 //agregar elementos en el array
-series.unshift('Vikings',2024);
+series.unshift("Vikings", 2024);
+mostrarSeries();
 
-document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
+//agregar un elemento al final del array
+series.push("Breaking bad");
+mostrarSeries();
 
-  //agregar un elemento al final del array
-  series.push('Breaking bad');
-  
-  document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
+// agregar elementos en el medio del array
+series.splice(3, 0, "Avatar");
+mostrarSeries();
 
-  // agregar elementos en el medio del array
-  series.splice(3,0,'Avatar');
-  document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
+// metodos para borrar elementos del array
+series.shift();
+mostrarSeries();
 
-  // metodos para borrar elementos del array
-  series.shift();
- 
-   document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
+//borrar el ultimo elemento del array
+series.pop();
+mostrarSeries();
 
-  //borrar el ultimo elemento del array
-  series.pop();
+//borrar elementos del medio del array
+// series.splice(5,1);
+// series.splice(5,2);
+series.splice(5);
+mostrarSeries();
 
-  document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
-
-  //borrar elementos del medio del array
-  // series.splice(5,1);
-  // series.splice(5,2);
-  series.splice(5);
-
-    document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
-
-  // modificar un elemento el array
-  series[3] = 'pokemon';
-  document.write(
-    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
-  );
-  document.write(`<ul>`);
-  for(let i=0; i < series.length; i++){
-      document.write(`<li>${series[i]}</li>`);
-  }
-  document.write(`</ul>`);
+// modificar un elemento el array
+series[3] = "pokemon";
+mostrarSeries();
