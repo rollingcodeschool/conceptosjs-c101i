f2f43f8084d0dfcf460fdbdb63f61672ea2c7d50
Emilse Arias <emilsearias@gmail.com>
2024-07-18 15:13:26 -0300

    agregamos metodos para trabajar con arrays

--- js/05-arrays.js
+++ js/05-arrays.js
@@ -56,3 +56,63 @@ document.write(
       document.write(`<li>${series[i]}</li>`);
   }
   document.write(`</ul>`);
+
+  // agregar elementos en el medio del array
+  series.splice(3,0,'Avatar');
+  document.write(
+    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
+  );
+  document.write(`<ul>`);
+  for(let i=0; i < series.length; i++){
+      document.write(`<li>${series[i]}</li>`);
+  }
+  document.write(`</ul>`);
+
+  // metodos para borrar elementos del array
+  series.shift();
+ 
+   document.write(
+    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
+  );
+  document.write(`<ul>`);
+  for(let i=0; i < series.length; i++){
+      document.write(`<li>${series[i]}</li>`);
+  }
+  document.write(`</ul>`);
+
+  //borrar el ultimo elemento del array
+  series.pop();
+
+  document.write(
+    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
+  );
+  document.write(`<ul>`);
+  for(let i=0; i < series.length; i++){
+      document.write(`<li>${series[i]}</li>`);
+  }
+  document.write(`</ul>`);
+
+  //borrar elementos del medio del array
+  // series.splice(5,1);
+  // series.splice(5,2);
+  series.splice(5);
+
+    document.write(
+    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
+  );
+  document.write(`<ul>`);
+  for(let i=0; i < series.length; i++){
+      document.write(`<li>${series[i]}</li>`);
+  }
+  document.write(`</ul>`);
+
+  // modificar un elemento el array
+  series[3] = 'pokemon';
+  document.write(
+    `<h2>Array de series: cantidad de elementos ${series.length}</h2>`
+  );
+  document.write(`<ul>`);
+  for(let i=0; i < series.length; i++){
+      document.write(`<li>${series[i]}</li>`);
+  }
+  document.write(`</ul>`);
