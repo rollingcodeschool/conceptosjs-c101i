11adc02c5967c2b241be4ca9a7b161d674dab4cc
Emilse Arias <emilsearias@gmail.com>
2024-07-23 16:33:56 -0300

    agregamos un objeto con notación literal

---
+++ js/07-objetos.js
@@ -0,0 +1,11 @@
+//notacion literal
+
+const vuelve = {
+    //propiedades
+    artista: 'Ricky Martin',
+    titulo: 'vuelve',
+    genero: ['balada', 'Pop'],
+    anio: 1998,
+    autor: 'franco de Vita'
+    //metodos
+}
